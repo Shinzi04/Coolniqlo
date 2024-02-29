1bc0b523372fc9b0949a89c6564806a509a86964
felixfrdd <148837128+felixfrdd@users.noreply.github.com>
2024-02-29 22:51:19 +0700

    Fixing conflict joo-coolniqlo branch to main

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 angular.module('olshop', [])
-  .controller('Controller', function($scope) {
+  .controller('Controller', function ($scope) {
     $scope.items = [
-      { name: "Item 1", tags: ["#new", "#trendy"], image: "celana1.jpg"},
+      { name: "Item 1", tags: ["#new", "#babi", "Item 1"], image: "celana1.jpg" },
       { name: "Item 2", tags: ["#old"], image: "celana1.jpg" },
       { name: "Item 3", tags: ["#new", "#trendy"], image: "celana1.jpg" },
+      { name: "Item 4", tags: ["#old"], image: "celana1.jpg" },
+      { name: "Item 5", tags: ["#new", "#trendy"], image: "celana1.jpg" },
+      { name: "Item 3", tags: ["#new", "#trendy"], image: "celana1.jpg" },
+      { name: "Item 4", tags: ["#old"], image: "celana1.jpg" },
+      { name: "Item 5", tags: ["#new", "#trendy"], image: "celana1.jpg" },
+      { name: "Item 3", tags: ["#new", "#trendy"], image: "celana1.jpg" },
+      { name: "Item 4", tags: ["#old"], image: "celana1.jpg" },
+      { name: "Item 5", tags: ["#new", "#trendy"], image: "celana1.jpg" },
+      { name: "Item 3", tags: ["#new", "#trendy"], image: "celana1.jpg" },
+      { name: "Item 4", tags: ["#old"], image: "celana1.jpg" },
+      { name: "Item 5", tags: ["#new", "#trendy"], image: "celana1.jpg" },
+
     ];
 
     $scope.searchText = "";
 
-    $scope.filterItems = function() {
+    $scope.filterItems = function () {
       return $scope.items.filter(item => {
         return item.tags.some(tag => tag.toLowerCase().includes($scope.searchText.toLowerCase()));
       });
