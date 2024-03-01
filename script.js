4504b04dcc61135221191a28aec8d7d4f98b65ef
JonathanKenn <JoKennWork04@gmail.com>
2024-03-01 22:00:43 +0700

    memperbaiki ui dan menambahkan buy button

--- script.js
+++ script.js
@@ -1,127 +1,130 @@
-angular.module("olshop", []).controller("Controller", function ($scope) {
+angular.module('olshop', []).controller('Controller', function ($scope) {
   $scope.items = [
     {
-      name: "Celana Panjang Hitam",
-      tags: ["Celana Panjang Hitam"],
+      name: 'Celana Panjang Hitam',
+      tags: ['Celana Panjang Hitam'],
       price: 150000,
       rating: 4,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
+      link: ' ',
     },
     {
-      name: "Sepatu Pantofel",
-      tags: ["#sepatu pantofel, #hitam"],
+      name: 'Sepatu Pantofel',
+      tags: ['#sepatu pantofel, #hitam'],
       price: 250000,
       rating: 3,
-      image: "sepatu_pantofel_1.jpg",
+      image: 'sepatu_pantofel_1.jpg',
+      link: ' ',
     },
     {
-      name: "Baju Kemeja Putih",
-      tags: ["#baju", "#kemeja putih", ""],
+      name: 'Baju Kemeja Putih',
+      tags: ['#baju', '#kemeja putih', ''],
       price: 125000,
       rating: 5,
-      image: "baju_kemeja_putih_1.jpg",
+      image: 'baju_kemeja_putih_1.jpg',
+      link: ' ',
     },
     {
-      name: "Item 4",
-      tags: ["#old"],
+      name: 'Item 4',
+      tags: ['#old'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 5",
-      tags: ["#new", "#trendy"],
+      name: 'Item 5',
+      tags: ['#new', '#trendy'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 3",
-      tags: ["#new", "#trendy"],
+      name: 'Item 3',
+      tags: ['#new', '#trendy'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 4",
-      tags: ["#old"],
+      name: 'Item 4',
+      tags: ['#old'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 5",
-      tags: ["#new", "#trendy"],
+      name: 'Item 5',
+      tags: ['#new', '#trendy'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 3",
-      tags: ["#new", "#trendy"],
+      name: 'Item 3',
+      tags: ['#new', '#trendy'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 4",
-      tags: ["#old"],
+      name: 'Item 4',
+      tags: ['#old'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 5",
-      tags: ["#new", "#trendy"],
+      name: 'Item 5',
+      tags: ['#new', '#trendy'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 3",
-      tags: ["#new", "#trendy"],
+      name: 'Item 3',
+      tags: ['#new', '#trendy'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 4",
-      tags: ["#old"],
+      name: 'Item 4',
+      tags: ['#old'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 5",
-      tags: ["#new", "#trendy"],
+      name: 'Item 5',
+      tags: ['#new', '#trendy'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 3",
-      tags: ["#new", "#trendy"],
+      name: 'Item 3',
+      tags: ['#new', '#trendy'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "Item 4",
-      tags: ["#old"],
+      name: 'Item 4',
+      tags: ['#old'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
     {
-      name: "TesterItem",
-      tags: ["#new", "#testeritem"],
+      name: 'TesterItem',
+      tags: ['#new', '#testeritem'],
       price: 150000,
       rating: 3,
-      image: "celana1.jpg",
+      image: 'celana1.jpg',
     },
   ];
-  
-  $scope.searchText = "";
+
+  $scope.searchText = '';
   $scope.showSearch = false;
 
   $scope.toggleSearch = function () {
@@ -129,18 +132,15 @@ angular.module("olshop", []).controller("Controller", function ($scope) {
   };
 
   $scope.clearSearch = function () {
-    $scope.searchText = "";
+    $scope.searchText = '';
   };
 
   $scope.filterItems = function () {
     const filteredItems = $scope.items.filter((item) => {
-      return item.tags.some((tag) =>
-      tag.toLowerCase().includes($scope.searchText.toLowerCase())
-      );
-    })
+      return item.tags.some((tag) => tag.toLowerCase().includes($scope.searchText.toLowerCase()));
+    });
 
-    const topItems = filteredItems.slice(0,15);
+    const topItems = filteredItems.slice(0, 15);
     return topItems;
   };
-
 });
