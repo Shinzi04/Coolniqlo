936864aa9334d60ba52dbf802e6bf1547b977c57
Shinzi <shinzisaja@gmail.com>
2024-03-01 14:01:37 +0700

    tampilan item diperbarui

--- script.js
+++ script.js
@@ -1,28 +1,115 @@
-angular.module('olshop', [])
-  .controller('Controller', function ($scope) {
-    $scope.items = [
-      { name: "Item 1", tags: ["#new", "#babi", "Item 1"], image: "celana1.jpg" },
-      { name: "Item 2", tags: ["#old"], image: "celana1.jpg" },
-      { name: "Item 3", tags: ["#new", "#trendy"], image: "celana1.jpg" },
-      { name: "Item 4", tags: ["#old"], image: "celana1.jpg" },
-      { name: "Item 5", tags: ["#new", "#trendy"], image: "celana1.jpg" },
-      { name: "Item 3", tags: ["#new", "#trendy"], image: "celana1.jpg" },
-      { name: "Item 4", tags: ["#old"], image: "celana1.jpg" },
-      { name: "Item 5", tags: ["#new", "#trendy"], image: "celana1.jpg" },
-      { name: "Item 3", tags: ["#new", "#trendy"], image: "celana1.jpg" },
-      { name: "Item 4", tags: ["#old"], image: "celana1.jpg" },
-      { name: "Item 5", tags: ["#new", "#trendy"], image: "celana1.jpg" },
-      { name: "Item 3", tags: ["#new", "#trendy"], image: "celana1.jpg" },
-      { name: "Item 4", tags: ["#old"], image: "celana1.jpg" },
-      { name: "Item 5", tags: ["#new", "#trendy"], image: "celana1.jpg" },
+angular.module("olshop", []).controller("Controller", function ($scope) {
+  $scope.items = [
+    {
+      name: "Celana Panjang Hitam",
+      tags: ["Celana Panjang Hitam"],
+      price: 150000,
+      rating: 4,
+      image: "celana1.jpg",
+    },
+    {
+      name: "Sepatu Pantofel",
+      tags: ["#sepatu pantofel, #hitam"],
+      price: 250000,
+      rating: 3,
+      image: "sepatu_pantofel_1.jpg",
+    },
+    {
+      name: "Baju Kemeja Putih",
+      tags: ["#baju", "#kemeja putih", ""],
+      price: 125000,
+      rating: 5,
+      image: "baju_kemeja_putih_1.jpg",
+    },
+    {
+      name: "Item 4",
+      tags: ["#old"],
+      price: 150000,
+      rating: 3,
+      image: "celana1.jpg",
+    },
+    {
+      name: "Item 5",
+      tags: ["#new", "#trendy"],
+      price: 150000,
+      rating: 3,
+      image: "celana1.jpg",
+    },
+    {
+      name: "Item 3",
+      tags: ["#new", "#trendy"],
+      price: 150000,
+      rating: 3,
+      image: "celana1.jpg",
+    },
+    {
+      name: "Item 4",
+      tags: ["#old"],
+      price: 150000,
+      rating: 3,
+      image: "celana1.jpg",
+    },
+    {
+      name: "Item 5",
+      tags: ["#new", "#trendy"],
+      price: 150000,
+      rating: 3,
+      image: "celana1.jpg",
+    },
+    {
+      name: "Item 3",
+      tags: ["#new", "#trendy"],
+      price: 150000,
+      rating: 3,
+      image: "celana1.jpg",
+    },
+    {
+      name: "Item 4",
+      tags: ["#old"],
+      price: 150000,
+      rating: 3,
+      image: "celana1.jpg",
+    },
+    {
+      name: "Item 5",
+      tags: ["#new", "#trendy"],
+      price: 150000,
+      rating: 3,
+      image: "celana1.jpg",
+    },
+    {
+      name: "Item 3",
+      tags: ["#new", "#trendy"],
+      price: 150000,
+      rating: 3,
+      image: "celana1.jpg",
+    },
+    {
+      name: "Item 4",
+      tags: ["#old"],
+      price: 150000,
+      rating: 3,
+      image: "celana1.jpg",
+    },
+    {
+      name: "Item 5",
+      tags: ["#new", "#trendy"],
+      price: 150000,
+      rating: 3,
+      image: "celana1.jpg",
+    },
+  ];
 
-    ];
+  $scope.searchText = "";
 
-    $scope.searchText = "";
-
-    $scope.filterItems = function () {
-      return $scope.items.filter(item => {
-        return item.tags.some(tag => tag.toLowerCase().includes($scope.searchText.toLowerCase()));
-      });
-    };
-  });
+  $scope.filterItems = function () {
+    return $scope.items.filter((item) => {
+      return item.tags.some((tag) =>
+        tag.toLowerCase().includes($scope.searchText.toLowerCase())
+      );
+    });
+  };
+  $scope.setRating = function (newRating) {
+    $scope.rating = newRating;
+  };
+});
