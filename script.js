2201684ad0fbbe57d09d29960fd2ddcf72068bfe
Gabs <kizaki705@gmail.com>
2024-03-04 07:57:22 +0700

    perbaiki error sedikit

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-angular.module("olshop", []).controller("Controller", function ($scope) {
+angular.module('olshop', []).controller('Controller', function ($scope) {
   $scope.items = [
     {
-      name: "Celana Panjang Hitam",
-      tags: ["Celana Panjang Hitam"],
+      name: 'Celana Panjang Hitam',
+      tags: ['#Celana Panjang Hitam'],
       price: 150000,
       rating: 4,
       image: "celana1.jpg",
       page: "nextPage/item1.html",
     },
     {
-      name: "Sepatu Pantofel",
-      tags: ["#sepatu pantofel, #hitam"],
+      name: 'Sepatu Pantofel',
+      tags: ['#Sepatu Pantofel'],
       price: 250000,
       rating: 3,
       image: "sepatupantofel.jpg",
       page: "nextPage/item2.html",
     },
     {
-      name: "Baju Kemeja Putih",
-      tags: ["#baju", "#kemeja putih", ""],
+      name: 'Baju Kemeja Putih',
+      tags: ['#Baju Kemeja Putih'],
       price: 125000,
       rating: 5,
       image: "baju_kemeja_putih_1.jpg",
@@ -26,7 +26,7 @@ angular.module("olshop", []).controller("Controller", function ($scope) {
     },
     {
       name: "Kemeja Kotak Hijau",
-      tags: ["#old"],
+      tags: ["#Kemeja Kotak Hijau"],
       price: 150000,
       rating: 3,
       image: "kemeja_kotak_hijau.jpg",
@@ -34,7 +34,7 @@ angular.module("olshop", []).controller("Controller", function ($scope) {
     },
     {
       name: "Kaus Putih Polos",
-      tags: ["#new", "#trendy"],
+      tags: ['#Kaus Putih Polos'],
       price: 88000,
       rating: 3,
       image: "kaosputih.jpg",
@@ -42,7 +42,7 @@ angular.module("olshop", []).controller("Controller", function ($scope) {
     },
     {
       name: "Celana Pendek Hitam",
-      tags: ["#new", "#trendy"],
+      tags: ['Celana Pendek Hitam'],
       price: 45000,
       rating: 3,
       image: "celanapendekhitam.jpg",
@@ -50,7 +50,7 @@ angular.module("olshop", []).controller("Controller", function ($scope) {
     },
     {
       name: "Celana Jeans Denim",
-      tags: ["#old"],
+      tags: ['Celana Jeans Denim'],
       price: 200000,
       rating: 5,
       image: "celanajeansdenim.jpg",
@@ -58,7 +58,7 @@ angular.module("olshop", []).controller("Controller", function ($scope) {
     },
     {
       name: "Dasi Hitam Polos",
-      tags: ["#new", "#trendy"],
+      tags: ['Dasi Hitam Polos'],
       price: 30000,
       rating: 4,
       image: "dasihitam.jpg",
@@ -66,7 +66,7 @@ angular.module("olshop", []).controller("Controller", function ($scope) {
     },
     {
       name: "Kaus Kaki Hitam",
-      tags: ["#new", "#trendy"],
+      tags: ['Kaus Kaki Hitam'],
       price: 18000,
       rating: 5,
       image: "kauskaki.jpg",
@@ -74,15 +74,16 @@ angular.module("olshop", []).controller("Controller", function ($scope) {
     },
     {
       name: "Kacamata Hitam",
-      tags: ["#old"],
+      tags: ['Kacamata Hitam'],
       price: 64000,
       rating: 5,
       image: "kacamatahitam.jpeg",
       page: "nextPage/item10.html",
     },
   ];
-  
-  $scope.searchText = "";
+
+
+  $scope.searchText = '';
   $scope.showSearch = false;
 
   $scope.toggleSearch = function () {
@@ -90,12 +91,15 @@ angular.module("olshop", []).controller("Controller", function ($scope) {
   };
 
   $scope.clearSearch = function () {
-    $scope.searchText = "";
+    $scope.searchText = '';
   };
 
-    $scope.filterItems = function () {
-      return $scope.items.filter(item => {
-        return item.tags.some(tag => tag.toLowerCase().includes($scope.searchText.toLowerCase()));
-      });
-    };
+  $scope.filterItems = function () {
+    const filteredItems = $scope.items.filter((item) => {
+      return item.tags.some((tag) => tag.toLowerCase().includes($scope.searchText.toLowerCase()));
+    });
+
+    const topItems = filteredItems.slice(0, 15);
+    return topItems;
+  };
   });
