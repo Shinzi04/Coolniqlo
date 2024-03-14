f46dd699276285bbef53795540841914716527c0
JonathanKenn <JoKennWork04@gmail.com>
2024-03-14 16:46:43 +0700

    convert to ejs?

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const ejs = require('ejs');
 
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
 
-app.listen(port, () =>  {
-    console.log(`Example app listening at http://localhost:${port}!`)
-})
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
