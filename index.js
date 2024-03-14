ffa43a6b3b23cc76bf235d61fc68ca8d3c095f53
felixfrdd <148837128+felixfrdd@users.noreply.github.com>
2024-03-15 02:52:50 +0700

    Added layouts (EJS) and dynamic page for product details

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const fs = require("fs");
+const products = JSON.parse(fs.readFileSync("products.json", "utf8"));
+const port = 3000;
 
-app.use(express.static('public'))
+app.set("view engine", "ejs");
 
-app.listen(port, () =>  {
-    console.log(`Example app listening at http://localhost:${port}!`)
-})
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/detail/:productID", (req, res) => {
+  const productID = req.params.productID;
+  const productData = products[productID];
+  if (productData) {
+    res.render("product-details", { productData });
+  } else {
+    res.send("Product does not exist!");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}!`);
+});
--- public/script.js
+++ public/script.js
@@ -1,151 +1,136 @@
-angular.module('olshop', []).controller('Controller', function ($scope) {
+angular.module("olshop", []).controller("Controller", function ($scope) {
   $scope.items = [
     {
-      name: 'Celana Panjang Hitam',
-      tags: ['#Celana Panjang Hitam'],
+      name: "Celana Panjang Hitam",
+      tags: ["#Celana Panjang Hitam"],
       price: 150000,
       rating: 4,
-      image: 'celana1.jpg',
-      page: 'nextPage/item1.html',
-      history: 'Terjual 130+',
+      image: "celana1.jpg",
+      page: "/detail/item1",
     },
     {
-      name: 'Sepatu Pantofel',
-      tags: ['#Sepatu Pantofel'],
+      name: "Sepatu Pantofel",
+      tags: ["#Sepatu Pantofel"],
       price: 250000,
       rating: 5,
-      image: 'sepatupantofel.jpg',
-      page: 'nextPage/item2.html',
-      history: 'Terjual 30+',
+      image: "sepatupantofel.jpg",
+      page: "/detail/item2",
     },
     {
-      name: 'Kemeja Putih Lengan Pendek',
-      tags: ['#Kemeja Putih Lengan Pendek'],
+      name: "Kemeja Putih Lengan Pendek",
+      tags: ["#Kemeja Putih Lengan Pendek"],
       price: 125000,
       rating: 5,
-      image: 'baju_kemeja_putih_1.jpg',
-      page: 'nextPage/item3.html',
-      history: 'Terjual 432+',
+      image: "baju_kemeja_putih_1.jpg",
+      page: "/detail/item3",
     },
     {
-      name: 'Kemeja Flanel Motif Kotak',
-      tags: ['#Kemeja Flanel Motif Kotak'],
+      name: "Kemeja Flanel Motif Kotak",
+      tags: ["#Kemeja Flanel Motif Kotak"],
       price: 150000,
       rating: 3,
-      image: 'kemeja_kotak_hijau.jpg',
-      page: 'nextPage/item4.html',
-      history: 'Terjual 9+',
+      image: "kemeja_kotak_hijau.jpg",
+      page: "/detail/item4",
     },
     {
-      name: 'Kaus Putih Polos',
-      tags: ['#Kaus Putih Polos'],
+      name: "Kaus Putih Polos",
+      tags: ["#Kaus Putih Polos"],
       price: 88000,
       rating: 3,
-      image: 'kaosputih.jpg',
-      page: 'nextPage/item5.html',
-      history: 'Terjual 130+',
+      image: "kaosputih.jpg",
+      page: "/detail/item5",
     },
     {
-      name: 'Celana Pendek Hitam',
-      tags: ['Celana Pendek Hitam'],
+      name: "Celana Pendek Hitam",
+      tags: ["Celana Pendek Hitam"],
       price: 45000,
       rating: 3,
-      image: 'celanapendekhitam.jpg',
-      page: 'nextPage/item6.html',
-      history: 'Terjual 3+',
+      image: "celanapendekhitam.jpg",
+      page: "/detail/item6",
     },
     {
-      name: 'Celana Jeans Denim',
-      tags: ['Celana Jeans Denim'],
+      name: "Celana Jeans Denim",
+      tags: ["Celana Jeans Denim"],
       price: 200000,
       rating: 5,
-      image: 'celanajeansdenim.jpg',
-      page: 'nextPage/item7.html',
-      history: 'Terjual 146+',
+      image: "celanajeansdenim.jpg",
+      page: "/detail/item7",
     },
     {
-      name: 'Dasi Hitam Polos',
-      tags: ['Dasi Hitam Polos'],
+      name: "Dasi Hitam Polos",
+      tags: ["Dasi Hitam Polos"],
       price: 30000,
       rating: 4,
-      image: 'dasihitam.jpg',
-      page: 'nextPage/item8.html',
-      history: 'Terjual 34+',
+      image: "dasihitam.jpg",
+      page: "/detail/item8",
     },
     {
-      name: 'Kaus Kaki Hitam Pendek',
-      tags: ['Kaus Kaki Hitam Pendek'],
+      name: "Kaus Kaki Hitam Pendek",
+      tags: ["Kaus Kaki Hitam Pendek"],
       price: 18000,
       rating: 5,
-      image: 'kauskaki.jpg',
-      page: 'nextPage/item9.html',
-      history: 'Terjual 534+',
+      image: "kauskaki.jpg",
+      page: "/detail/item9",
     },
     {
-      name: 'Kacamata Hitam',
-      tags: ['Kacamata Hitam'],
+      name: "Kacamata Hitam",
+      tags: ["Kacamata Hitam"],
       price: 64000,
       rating: 5,
-      image: 'kacamatahitam.jpeg',
-      page: 'nextPage/item10.html',
-      history: 'Terjual 76+',
+      image: "kacamatahitam.jpeg",
+      page: "/detail/item10",
     },
     {
-      name: 'Mantel Berkerah',
-      tags: ['Mantel Berkerah'],
+      name: "Mantel Berkerah",
+      tags: ["Mantel Berkerah"],
       price: 400000,
       rating: 5,
-      image: 'mantel_berkerah.jpg',
-      page: 'nextPage/item11.html',
-      history: 'Terjual 3+',
+      image: "mantel_berkerah.jpg",
+      page: "/detail/item11",
     },
     {
-      name: 'Tas Bahu',
-      tags: ['Tas Bahu'],
+      name: "Tas Bahu",
+      tags: ["Tas Bahu"],
       price: 140000,
       rating: 4,
-      image: 'tas_bahu.jpg',
-      page: 'nextPage/item12.html',
-      history: 'Terjual 12+',
+      image: "tas_bahu.jpg",
+      page: "/detail/item12",
     },
     {
-      name: 'Kemeja Pastel',
-      tags: ['Kemeja Pastel'],
+      name: "Kemeja Pastel",
+      tags: ["Kemeja Pastel"],
       price: 145000,
       rating: 4,
-      image: 'kemeja_pastel.jpg',
-      page: 'nextPage/item13.html',
-      history: 'Terjual 8+',
+      image: "kemeja_pastel.jpg",
+      page: "/detail/item13",
     },
     {
-      name: 'Rok Panjang',
-      tags: ['Rok Panjang'],
+      name: "Rok Panjang",
+      tags: ["Rok Panjang"],
       price: 200000,
       rating: 5,
-      image: 'rok_panjang.jpg',
-      page: 'nextPage/item14.html',
-      history: 'Terjual 80+',
+      image: "rok_panjang.jpg",
+      page: "/detail/item14",
     },
     {
-      name: 'Kardigan',
-      tags: ['Kardigan'],
+      name: "Kardigan",
+      tags: ["Kardigan"],
       price: 250000,
       rating: 5,
-      image: 'kardigan.jpg',
-      page: 'nextPage/item15.html',
-      history: 'Terjual 21+',
+      image: "kardigan.jpg",
+      page: "/detail/item15",
     },
     {
-      name: 'Tester Item',
-      tags: ['Tester Item'],
+      name: "Tester Item",
+      tags: ["Tester Item"],
       price: 99999999999,
       rating: 5,
-      image: 'rickroll.jpg',
-      page: '',
+      image: "rickroll.jpg",
+      page: "",
     },
   ];
 
-  $scope.searchText = '';
+  $scope.searchText = "";
   $scope.showSearch = false;
 
   $scope.toggleSearch = function () {
@@ -153,12 +138,14 @@ angular.module('olshop', []).controller('Controller', function ($scope) {
   };
 
   $scope.clearSearch = function () {
-    $scope.searchText = '';
+    $scope.searchText = "";
   };
 
   $scope.filterItems = function () {
     const filteredItems = $scope.items.filter((item) => {
-      return item.tags.some((tag) => tag.toLowerCase().includes($scope.searchText.toLowerCase()));
+      return item.tags.some((tag) =>
+        tag.toLowerCase().includes($scope.searchText.toLowerCase())
+      );
     });
 
     const topItems = filteredItems.slice(0, 15);
@@ -167,9 +154,9 @@ angular.module('olshop', []).controller('Controller', function ($scope) {
 });
 
 //Toggle class active utk hamburger menu
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 
 //ketika humburger menu diklik (ARROW FUNCTION)
-document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
