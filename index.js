fab5b2b416af7a652780d410e0f07da7e774d498
JonathanKenn <JoKennWork04@gmail.com>
2024-03-29 20:01:30 +0700

    tambahin login n regis page

--- index.js
+++ index.js
@@ -12,29 +12,28 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index' , {products:items.slice(0,15)});
+  res.render('index', { products: items.slice(0, 15) });
 });
 
-app.get('/search' , (req,res) =>{
+app.get('/search', (req, res) => {
   const query = req.query.q;
-  if(!query){
-    res.render('index', {products:items.slice(0,15)});
+  if (!query) {
+    res.render('index', { products: items.slice(0, 15) });
     return;
   }
 
-  const filteredItems = searchItems(items,query).slice(0,15);
-  res.render('index', {products:filteredItems})
+  const filteredItems = searchItems(items, query).slice(0, 15);
+  res.render('index', { products: filteredItems });
+});
 
-})
-
-app.get("/detail/:productID", (req, res) => {
+app.get('/detail/:productID', (req, res) => {
   const productID = req.params.productID;
-  const productData = items.find(product => product.id === productID);
+  const productData = items.find((product) => product.id === productID);
   if (productData) {
-    res.render("product-details", { productData });
+    res.render('product-details', { productData });
   } else {
-    res.status(404).send("Product not found");
-  }
+    res.status(404).send('Product not found');
+  }
 });
 
 app.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -1,27 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const searchToggle = document.getElementById("searchToggle");
-  const searchIcon = document.getElementById("searchIcon");
-  const closeIcon = document.getElementById("closeIcon");
-  const searchBar = document.getElementById("searchBar");
+document.addEventListener('DOMContentLoaded', function () {
+  const searchToggle = document.getElementById('searchToggle');
+  const searchIcon = document.getElementById('searchIcon');
+  const closeIcon = document.getElementById('closeIcon');
+  const searchBar = document.getElementById('searchBar');
 
-  searchToggle.addEventListener("click", function () {
-    toggleSearch();
+  searchToggle.addEventListener('click', function () {
+    searchBar.classList.toggle('noSearch');
+    if (searchBar.classList.contains('noSearch')) {
+      searchIcon.style.display = 'none';
+      closeIcon.style.display = 'block';
+    } else {
+      searchIcon.style.display = 'block';
+      closeIcon.style.display = 'none';
+    }
   });
-
-  function toggleSearch() {
-    searchIcon.style.display =
-      searchIcon.style.display === "none" ? "inline" : "none";
-    closeIcon.style.display =
-      closeIcon.style.display === "none" ? "inline" : "none";
-    searchBar.style.display =
-      closeIcon.style.display === "none" ? "inline" : "none";
-  }
 });
 
 //Toggle class active utk hamburger menu
-const navbarNav = document.querySelector(".navbar-nav");
+const navbarNav = document.querySelector('.navbar-nav');
 
 //ketika humburger menu diklik (ARROW FUNCTION)
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
 };
