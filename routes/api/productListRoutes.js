9982f233d687b37577badf22a2dc657261fd1dd1
felixfrdd <148837128+felixfrdd@users.noreply.github.com>
2024-04-01 01:12:10 +0700

    Added method post for products with image (multer), changed /assets/Items structure,

--- routes/api/productListRoutes.js
+++ routes/api/productListRoutes.js
@@ -1,8 +1,29 @@
 const { Router } = require("express");
 const router = Router();
 const Product = require("../../models/productList");
+const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
 
-// method get (READ)
+// konfigurasi multer untuk handling file yang di upload
+const storage = multer.diskStorage({
+  destination: async (req, file, cb) => {
+    try {
+      const productId = req.body.id;
+      const dir = `public/assets/Items/${productId}`;
+      await fs.promises.mkdir(dir, { recursive: true });
+      cb(null, dir);
+    } catch (error) {
+      cb(error);
+    }
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+// method get (READ) untuk mendapatkan list produk
 router.get("/", async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
@@ -13,7 +34,7 @@ router.get("/", async (req, res) => {
     res.render("dashboard", {
       products,
       title: "Manage Products - Coolniqlo",
-      style: "../dashboard.css",
+      style: "../css/dashboard.css",
       currentPage: page,
       totalPages: Math.ceil(totalProducts / limit),
     });
@@ -22,22 +43,51 @@ router.get("/", async (req, res) => {
   }
 });
 
-// method post (CREATE)
-router.post("/add", async (req, res) => {
-  try {
-    const existingProduct = await Product.findOne({ id: req.body.id });
-    if (existingProduct) {
-      return res.status(400).send("Product with this ID already exists");
-    }
-    const product = new Product(req.body);
-    await product.save();
-    res.redirect("/admin/dashboard");
-  } catch (error) {
-    res.status(500).send("Internal Server Error");
-  }
-});
+// method post (CREATE) untuk menambahkan produk
+router.post(
+  "/add",
+  upload.fields([
+    { name: "bigImage", maxCount: 1 },
+    { name: "smallImages", maxCount: 8 },
+  ]),
+  async (req, res) => {
+    try {
+      const productId = req.body.id;
 
-// method put (UPDATE)
+      // product ID harus unik
+      if (await Product.findOne({ id: productId })) {
+        return res.status(400).send("Product with this ID already exists");
+      }
+
+      const bigImage = req.files["bigImage"][0];
+      const smallImages = req.files["smallImages"];
+
+      // membuat array path smallImages
+      const smallImagePaths = await Promise.all(
+        smallImages.map(async (smallImage) => {
+          return `/../assets/Items/${productId}/${smallImage.originalname}`;
+        })
+      );
+
+      // mengurut array path smallImages secara ascending
+      smallImagePaths.sort();
+
+      await Product.save({
+        id: productId,
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        bigImage: `/../assets/Items/${productId}/${bigImage.originalname}`,
+        smallImages: smallImagePaths,
+      });
+      res.redirect("/admin/dashboard");
+    } catch (error) {
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+// method put (UPDATE) untuk perbarui produk
 router.put("/edit/:id", async (req, res) => {
   try {
     await Product.findByIdAndUpdate(req.params.id, {
@@ -54,12 +104,18 @@ router.put("/edit/:id", async (req, res) => {
   }
 });
 
-// method delete (DELETE)
+// method delete (DELETE) untuk menghapus produk dan juga menghapuskan file direktori image
 router.delete("/delete/:id", async (req, res) => {
   try {
-    await Product.findByIdAndDelete(req.params.id);
+    const product = await Product.findByIdAndDelete(req.params.id);
+    const productDir = path.join(
+      __dirname,
+      `/../../public/assets/Items/${product.id}`
+    );
+    fs.rmdirSync(productDir, { recursive: true });
     res.sendStatus(200);
   } catch (error) {
+    console.error("Error deleting product:", error);
     res.status(500).send("Internal Server Error");
   }
 });
