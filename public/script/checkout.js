0217dbfcc72b05d9c9acf6fbfc58a7cc2ded2547
JonathanKenn <JoKennWork04@gmail.com>
2024-05-05 22:22:27 +0700

    memperbaiki tampilan dashboard

--- public/script/checkout.js
+++ public/script/checkout.js
@@ -1,25 +1,3 @@
-// document.getElementById('checkout-form').addEventListener('submit', function (event) {
-//   // Mendapatkan nilai dari inputan
-//   let address = document.getElementById('address').value;
-//   let postalCode = document.getElementById('postal-code').value;
-//   let city = document.getElementById('city').value;
-
-//   // Memeriksa apakah semua input telah diisi
-//   if (address === '' || postalCode === '' || city === '') {
-//     alert('Please fill in all fields before checking out.');
-//     event.preventDefault(); // Mencegah pengiriman formulir jika input masih kosong
-//   } else {
-//     // Lakukan sesuatu jika semua input telah diisi
-//     console.log('Address:', address);
-//     console.log('Postal Code:', postalCode);
-//     console.log('City:', city);
-//     const popUpRecieve = document.querySelector('.popupRecieve');
-//     popUpRecieve.style.display = 'block';
-//     checkout(userID);
-//     // Lakukan proses checkout atau tindakan lainnya
-//   }
-// });
-
 document.addEventListener('DOMContentLoaded', function () {
   let inputs = document.querySelectorAll('input[required]');
   let checkoutBtn = document.querySelector('.checkoutBtn');
