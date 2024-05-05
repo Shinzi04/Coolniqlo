07cd56ea3454a0c1d76add01f54e0bb0637ab453
felixfrdd <148837128+felixfrdd@users.noreply.github.com>
2024-05-05 18:21:56 +0700

    add query to update products sold with quantity

--- README.md
+++ README.md
@@ -5,21 +5,28 @@ npm install
 npm start run
 ```
 ## Data
-- Import data **products.json** ke MongoDB untuk menggunakan data produk 
+- Import data **products.json** (/backup-data) ke MongoDB untuk menggunakan data produk 
   atau dengan menambahkan langsung pada Dashboard Admin.
-- Import data **admin.json** ke MongoDB untuk memasukan admin dan user/customer ke
+- Import data **admin.json** (/backup-data) ke MongoDB untuk memasukan admin dan user/customer ke
   aplikasi
 - Gambar produk ada di public/assets/Items
 - Backup data produk ada di folder backup-data 
-## Akun
+## Akun (butuh import data admin.json)
 - admin@gmail.com dengan password admin123
 - tester@gmail.com dengan password tester
-## Dashboard admin
+- Dapat Sign Up dengan memasukan email, password, dan kode verifikasi
+## User Customer
+- Customer harus registrasi akun sebelum menambahkan produk ke dalam keranjang (cart)
+- Penambahan produk ke cart ditambahkan melalui ikon cart pada atas pojok kiri thumbnail produk
+- Produk yang tidak 
+## Dashboard Admin
 - Dashboard (*/admin/dashboard*) **hanya** dapat diakses oleh admin
 - Gambar yang dimasukkan akan disimpan di public/uploads
 - Saat penambahan gambar produk *bigImage*, penambahan *smallImages* harus upload gambar
   yang sama dengan *bigImage* satu kali agar *bigImage* dapat dipilih saat detail produk
 - Penambahan produk pada saat add dan edit akan **menghapus** foto yang sebelumnya dipakai
+- Order list adalah daftar order yang dilakukan oleh customer dan diurutkan berdasarkan waktu dan tanggal beli
+- Sales adalah daftar banyaknya produk yang terjual
 ## Running
 - Gunakan **localhost:3000** pada browser untuk menjalankan aplikasi
 ## Contributor
--- routes/dashboard/salesRoutes.js
+++ routes/dashboard/salesRoutes.js
@@ -9,46 +9,17 @@ router.get("/sales", isAdmin, async (req, res) => {
     const page = parseInt(req.query.page) || 1;
     const limit = 10;
     const skip = (page - 1) * limit;
-
-    // total kuantitas produk berdasarkan ID produk
-    const totalQuantities = await purchaseHistory.aggregate([
-      {
-        $group: {
-          _id: "$productID",
-          totalQuantity: { $sum: "$quantity" },
-        },
-      },
-    ]);
-
-    // membuat map untuk menyimpan total kuantitas setiap produk
-    const totalQuantitiesMap = new Map();
-    totalQuantities.forEach((item) => {
-      totalQuantitiesMap.set(item._id.toString(), item.totalQuantity);
-    });
-
-    // query semua produk
-    let products = await Product.find();
-
-    // mengambil total kuantitas produk setiap produk dan menyimpan pada totalQuantity
-    products.forEach((product) => {
-      const totalQuantity = totalQuantitiesMap.get(product._id.toString()) || 0;
-      product.totalQuantity = totalQuantity;
-    });
-
-    // sortir total kuantitas produk
-    products.sort((a, b) => b.totalQuantity - a.totalQuantity);
-
-    // pagination setelah sorting
-    const totalProducts = products.length;
-    const totalPages = Math.ceil(totalProducts / limit);
-    products = products.slice(skip, skip + limit);
-
+    const products = await Product.find()
+      .sort({ sold: -1 })
+      .skip(skip)
+      .limit(limit);
+    const totalProducts = await Product.countDocuments();
     res.render("dashboard/sales", {
       products,
       title: "Sales - Coolniqlo",
       style: "../../css/dashboard.css",
       currentPage: page,
-      totalPages,
+      totalPages: Math.ceil(totalProducts / limit),
       activePage: "sales",
     });
   } catch (error) {
